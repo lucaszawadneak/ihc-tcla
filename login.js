64806d509f3eefdfb4f7079c46410ec448455650
Lucas Cassilha <lucaszawadneak@gmail.com>
2022-09-05 16:47:30 -0300

    login erro ao não colocar email e senha

---
+++ login.js
@@ -0,0 +1,10 @@
+
+function login() {
+      // if (document.getElementById("email").value.length > 0 && document.getElementById("password").value.length > 0) {
+      //   window.location.href = "c01-inscrito.html";
+      // } else {
+      document?.getElementById("alert")?.style.display = "block";
+      document?.getElementById("email")?.className += "  border-danger";
+      document?.getElementById("senha")?.className += "  border-danger";
+      // }
+}
